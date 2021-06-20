5ef988cf7ff2d3bb59da8d26318a70bd8bfb71dc
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-06-20 09:12:51 -0300

    Add Exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,18 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const calcButtonEl = document.querySelector('#calcular');
+const constanteEL = document.querySelector('#constante');
+const massa1El = document.querySelector('#massa1');
+const massa2El = document.querySelector('#massa2');
+const distanciaEL = document.querySelector('#distancia');
+const resultadoEl = document.querySelector('#resultado');
+
+calcButtonEl.addEventListener('click', calcGravitationalForce);
+
+function calcGravitationalForce(){
+    const constante = constanteEL.value;
+    const massa1 = massa1El.value;
+    const massa2 = massa2El.value;
+    const distancia = distanciaEL.value;
+
+    resultadoEl.value = (constante * massa1 * massa2)/(distancia ** 2); 
+}
